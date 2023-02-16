cc78dc8f70562ba8071e7b3eea01e6d0e2679075
Snehal979 <bansodsnehal535@gmail.com>
2023-02-16 08:09:23 +0530

    [Snehal]Add:Single delegate

--- DelegateAndEvent/Program.cs
+++ DelegateAndEvent/Program.cs
@@ -5,6 +5,18 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome Delegate and Event program");
+            Console.WriteLine("Enter a Number \n 1.single cast delegate");
+            int num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                case 1:
+                    SingleCastDelegate creat = new SingleCastDelegate();//Object
+                    Calculation del = new Calculation(creat.Add1);//delegate
+                    del(100, 40);
+                    Calculation del1 = new Calculation(creat.Sub1);//delegate
+                    del1(100, 40);
+                    break;
+            }
         }
     }
 }
---
+++ DelegateAndEvent/SingleCastDelegate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateAndEvent
+{
+    // DELEGATE MODIFER/DELEGATE/RETURN TYPE/NAME OF DELEGATE(PARAMETHER)
+    public delegate void Calculation(int a, int b);
+    /// <summary>
+    /// SingleCastDelegate
+    /// </summary>
+    public class SingleCastDelegate
+    {
+        public void Add1(int a, int b)
+        {
+            Console.WriteLine("sum is {0}", a + b);
+        }
+        public void Sub1(int a, int b)
+        {
+            Console.WriteLine("sub is {0}", a - b);
+        }
+
+    }
+}
